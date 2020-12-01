cf93815d3b0e8e3729089bc06ce508ed191c1112
sonthanhnguyen13 <sonsieunhan2098@gmail.com>
2020-12-01 19:05:46 +0700

    Updates

---
+++ precache-manifest.956398b17cd24765db894e80f2383e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba65c33e77696f42e7618362ac69679b",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "ab0ce81bc8558d29a5db",
+    "url": "/toDoList/static/css/main.bce72063.chunk.css"
+  },
+  {
+    "revision": "3aeaed96fcdbcfa11433",
+    "url": "/toDoList/static/js/2.042702ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.042702ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0ce81bc8558d29a5db",
+    "url": "/toDoList/static/js/main.bf317629.chunk.js"
+  },
+  {
+    "revision": "43be285d396891592ec1",
+    "url": "/toDoList/static/js/runtime-main.15e8d734.js"
+  }
+]);
